ae91fc7063ef7b3496862a19c3aa9548ec9a6642
AghinKhalidCP <86180193+AghinKhalidCP@users.noreply.github.com>
2021-10-16 14:01:57 +0530

    Add files via upload

--- DonLee_Robot/Translation.py
+++ DonLee_Robot/Translation.py
@@ -3,22 +3,18 @@ class Translation(object):
   
     START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
 
-<b>Am Just A Pro Auto Filter Bot....😉</b>
+<i>I'm a Powerful Movie Providing Bot🐽��</i>
 
-<b>Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝</b>
+<i>Wanna See it...?👀</i>
 
-<b>Subscribe to the update channel to learn about my updates and activity...</b>
-
-<b>Press /help to know about available commands🤪</b>
-
-<b>Maintained By @Mo_Tech_YT</b>"""    
+<b>Then,</b>
+Check👇
+@ADBZ_Filmworld"""    
     
     HELP_TEXT = """
-<b><u>Notice</u></b>
-<code>Imdb Poster Imdb is available on this bot
-Rating Not Available</codd> 
+<b>🙋‍♂️Hey {}!!</b>
 
-<b><u>Bot Commands (Works Only In Groups)</u></b>
+<b>Bot Commands (Works Only In Groups):</b>
 
 ☞ <code>/add chat_id</code> - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
   
@@ -35,19 +31,19 @@ Rating Not Available</codd>
    ☞ <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
             
    ☞ <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
-
-Maintained By @Mo_Tech_YT"""
+   
+<b>@adbz_filmworld</b>"""
     
     ABOUT_TEXT = """
-<b>➥ 🤖Bot</b> : <b>Adv Auto Filter Bot v2.9</b>
+<b>➥ 🤖Bot</b> : <b>Geralt Of ADBZ</b>
     
-<b>➥ 😎Creator</b> : <b>@AlbertEinstein_TG</b> 
-
-<b>➥ 👨‍💻Editor</b> : <b>@Mrk_YT</b>
+<b>➥ 😎Creator</b> : <b>@SultanMehmed_TG</b> 
 
 <b>➥ 🗣️Language</b> : <b>Python3</b>
 
 <b>➥ 📚Library</b> : <b>Pyrogram Asyncio 1.13.0</b>
 
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/PR0FESS0R-99/DonLee_Robot">💥Click Me</a></b>
+<b>➥ 🗳Server</b> : <b>🈂Heroku</b>
+
+</b>@adbz_filmworld</b>
 """
